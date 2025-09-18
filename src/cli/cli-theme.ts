6b9f018a98cd0c168da1cc2c095269bb14ecf881
Iesht <irinashteynfeld@gmail.com>
2025-09-18 14:30:31 +0500

    add CLI application with --help, --version and --import commands

--- src/cli/cli-theme.ts
+++ src/cli/cli-theme.ts
@@ -4,5 +4,5 @@ export const ERROR = chalk.bold.hex('#EF2D56');
 export const WARNING = chalk.hex('#ED7D3A');
 export const SUCCESS = chalk.bold.hex('#8CD867');
 export const INFO = chalk.hex('#73D2DE');
-export const COMMAND = chalk.bold.hex('#CBCBD4').bold;
+export const COMMAND = chalk.bold.hex('#CBCBD4');
 export const TITLE = chalk.underline.hex('#218380').bold;
