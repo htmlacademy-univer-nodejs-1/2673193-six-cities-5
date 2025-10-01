b0c0bffbda06596d5b225c898a9171f3ebd56154
Iesht <irinashteynfeld@gmail.com>
2025-10-01 16:41:47 +0500

    add TSVOfferGenerator, fix Coordinates for Offer type, fix eslint errors

--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -1,4 +1,4 @@
-import { Offer, OfferType, City, AmenityType, UserType } from '../../types/index.js';
+import { Offer, OfferType, City, AmenityType, UserType, CITY_COORDINATES } from '../../types/index.js';
 import { FileReader } from './file-reader.interface.js';
 import { readFileSync } from 'node:fs';
 
@@ -22,33 +22,37 @@ export class TSVFileReader implements FileReader {
       .split('\n')
       .filter((line) => line.trim().length > 0)
       .map((line) => line.split('\t'))
-      .map(([title, description, publicationDate, city, previewImage, images, isPremium, isFavourite, rating, type, bedrooms, maxGuests, price, amenities, authorName, authorEmail, authorAvatar, authorPassword, authorType, commentsCount, coordinates]): Offer => ({
-        title,
-        description,
-        publicationDate: new Date(publicationDate),
-        city: City[city as keyof typeof City],
-        previewImage,
-        images: images.split(';'),
-        isPremium: isPremium === 'true',
-        isFavorite: isFavourite === 'true',
-        rating: Number.parseFloat(rating),
-        type: type as OfferType,
-        bedrooms: Number.parseInt(bedrooms, 10),
-        maxGuests: Number.parseInt(maxGuests, 10),
-        price: Number.parseInt(price, 10),
-        amenities: amenities.split(';').map((amenity) => amenity as AmenityType),
-        author: {
-          name: authorName,
-          email: authorEmail,
-          avatar: authorAvatar,
-          password: authorPassword,
-          type: authorType as UserType,
-        },
-        commentsCount: parseInt(commentsCount, 10),
-        coordinates: {
-          latitude: Number.parseFloat(coordinates.split(',')[0]),
-          longitude: Number.parseFloat(coordinates.split(',')[1]),
-        },
-      }));
+      .map(([title, description, publicationDate, city, previewImage, images, isPremium, isFavourite, rating, type, bedrooms, maxGuests, price, amenities, authorName, authorEmail, authorAvatar, authorPassword, authorType, commentsCount, coordinates]): Offer => {
+        const validCity = Object.values(City).includes(city as City) ? (city as City) : City.Paris;
+        return {
+          title,
+          description,
+          publicationDate: new Date(publicationDate),
+          city: City[city as keyof typeof City],
+          previewImage,
+          images: images.split(';'),
+          isPremium: isPremium === 'true',
+          isFavorite: isFavourite === 'true',
+          rating: Number.parseFloat(rating),
+          type: type as OfferType,
+          bedrooms: Number.parseInt(bedrooms, 10),
+          maxGuests: Number.parseInt(maxGuests, 10),
+          price: Number.parseInt(price, 10),
+          amenities: amenities.split(';').map((amenity) => amenity as AmenityType),
+          author: {
+            name: authorName,
+            email: authorEmail,
+            avatar: authorAvatar,
+            password: authorPassword,
+            type: authorType as UserType,
+          },
+          commentsCount: parseInt(commentsCount, 10),
+          coordinates: coordinates ? {
+            latitude: Number.parseFloat(coordinates.split(',')[0]),
+            longitude: Number.parseFloat(coordinates.split(',')[1]),
+          }
+            : CITY_COORDINATES[validCity]
+        };
+      });
   }
 }
